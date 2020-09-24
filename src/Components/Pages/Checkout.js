4caddd37464ba6196b00753e3850cd965f46acbd
kendalldc <kdendtler@gmail.com>
2020-09-24 14:36:11 -0400

    update

--- src/Components/Pages/Checkout.js
+++ src/Components/Pages/Checkout.js
@@ -1,7 +1,27 @@
+// import React from 'react'
+// import StripeCheckout from 'react-stripe-checkout';
+
+//  class Checkout extends React.Component {
+//   onToken = (token, addresses) => {
+    
+//   };
+
+//   render() {
+//     return (
+//       <StripeCheckout
+//         stripeKey="pk_test_51HUOsdEFbh8Rt1oFQRRqit9GaPEiBrAJPqJVBUPm1vAyPFpJTCrex3vWo9X9NlyCIUhSzSOZdkanRrfzGV5A4dwT00gvZI8p59"
+//         token={this.onToken}
+//       />
+//     )
+//   }
+// }
+
+// export default Checkout;
+
+
 import React from 'react'
 import StripeCheckout from 'react-stripe-checkout';
-
-export default class Checkout extends React.Component {
+class Checkout extends React.Component {
   onToken = (token, addresses) => {
     
   };
@@ -9,9 +29,18 @@ export default class Checkout extends React.Component {
   render() {
     return (
       <StripeCheckout
+        amount="500"
+        billingAddress
+        description="Awesome Product"
+        image="https://yourdomain.tld/images/logo.svg"
+        locale="auto"
+        name="YourDomain.tld"
         stripeKey="pk_test_51HUOsdEFbh8Rt1oFQRRqit9GaPEiBrAJPqJVBUPm1vAyPFpJTCrex3vWo9X9NlyCIUhSzSOZdkanRrfzGV5A4dwT00gvZI8p59"
         token={this.onToken}
+        zipCode
       />
     )
   }
 }
+
+export default Checkout;
