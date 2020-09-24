dae9f69256e4e88b1aa68a5e988a9e2deec06709
kendalldc <kdendtler@gmail.com>
2020-09-24 13:24:24 -0400

    about css

--- src/Components/Pages/About.js
+++ src/Components/Pages/About.js
@@ -18,7 +18,7 @@ Did you know?
 8 million tonnes of plastic are dumped in our oceans every year, and each day around 7 million coffee cups are thrown away in the UK.</p>
 <button>Reduce Waste</button>
         </div>
-        <div class="forest">
+        {/* <div class="forest">
         <h3>Forest Friendly</h3>
             <p>It is estimated that around 600 million tonnes of products and materials enter the UK economy each year and only 115 million tonnes are recycled at the end of their life. Our Reducing Waste label offers an edit of refillables, alternatives to single-use plastic items and products which have been created by upcycling, recycling and regenerating waste into innovative new materials, components and ingredients.
 What can you do?
@@ -41,7 +41,7 @@ By saying no to single-use items and choosing products made from recycled materi
 Did you know?
 8 million tonnes of plastic are dumped in our oceans every year, and each day around 7 million coffee cups are thrown away in the UK.</p>
 <button>Reduce Waste</button>
-        </div>
+        </div> */}
       </div>
     )
 }
