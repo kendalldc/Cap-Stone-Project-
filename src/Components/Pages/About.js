7e697c669f9be3dd06a483ae8939980fe3041cab
kendalldc <kdendtler@gmail.com>
2020-09-24 13:19:16 -0400

    update about css

--- src/Components/Pages/About.js
+++ src/Components/Pages/About.js
@@ -4,7 +4,7 @@ function About(){
     return(
         <div class="about-container">
         <div class="Img-container">
-            <img src='https://images.squarespace-cdn.com/content/v1/52eb1f2fe4b03b5e919309d0/1506639034521-PKUBPT8MND4TPBF4KDC4/ke17ZwdGBToddI8pDm48kPTrHXgsMrSIMwe6YW3w1AZ7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0k5fwC0WRNFJBIXiBeNI5fKTrY37saURwPBw8fO2esROAxn-RKSrlQamlL27g22X2A/%21Screen+Grab+07.jpg?format=1500w'/>
+            <img  class="beach"src='https://images.squarespace-cdn.com/content/v1/52eb1f2fe4b03b5e919309d0/1506639034521-PKUBPT8MND4TPBF4KDC4/ke17ZwdGBToddI8pDm48kPTrHXgsMrSIMwe6YW3w1AZ7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0k5fwC0WRNFJBIXiBeNI5fKTrY37saURwPBw8fO2esROAxn-RKSrlQamlL27g22X2A/%21Screen+Grab+07.jpg?format=1500w'/>
         </div>
         <div class="waste">
             <h3>REDUCING WASTE</h3>
@@ -19,7 +19,7 @@ Did you know?
 <button>Reduce Waste</button>
         </div>
         <div class="forest">
-        <h3>REDUCING WASTE</h3>
+        <h3>Forest Friendly</h3>
             <p>It is estimated that around 600 million tonnes of products and materials enter the UK economy each year and only 115 million tonnes are recycled at the end of their life. Our Reducing Waste label offers an edit of refillables, alternatives to single-use plastic items and products which have been created by upcycling, recycling and regenerating waste into innovative new materials, components and ingredients.
 What can you do?
 - Purchase products made from recycled materials and ingredients
@@ -30,7 +30,18 @@ Did you know?
 8 million tonnes of plastic are dumped in our oceans every year, and each day around 7 million coffee cups are thrown away in the UK.</p>
 <button>Reduce Waste</button>
         </div>
-        <div class="support"></div>
+        <div class="support">
+        <h3>Supporting Our Communities</h3>
+            <p>It is estimated that around 600 million tonnes of products and materials enter the UK economy each year and only 115 million tonnes are recycled at the end of their life. Our Reducing Waste label offers an edit of refillables, alternatives to single-use plastic items and products which have been created by upcycling, recycling and regenerating waste into innovative new materials, components and ingredients.
+What can you do?
+- Purchase products made from recycled materials and ingredients
+- Use refillable products – washing liquid to perfume, the options are out there
+- Reduce your use of single-use plastic
+By saying no to single-use items and choosing products made from recycled materials, you’re helping to preserve natural resources.
+Did you know?
+8 million tonnes of plastic are dumped in our oceans every year, and each day around 7 million coffee cups are thrown away in the UK.</p>
+<button>Reduce Waste</button>
+        </div>
       </div>
     )
 }
